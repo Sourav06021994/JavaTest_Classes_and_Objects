6d215e2a38e6939d94e1117f7bfaa972fae17217
Sourav06021994 <71684782+Sourav06021994@users.noreply.github.com>
2020-09-22 09:51:07 +0530

    Update Bill.java

--- src/billing/Bill.java
+++ src/billing/Bill.java
@@ -1,6 +1,16 @@
 package billing;
 
 public class Bill {
+	ArrayList<String> items=new ArrayList<String>();
+	double totalprice=0.0;
+	public double getTotalBill()
+	{
+		return totalprice;
+	}
+  public void addToBill(double price)
+  {
+	 totalprice+=price; 
+  }
   
 	  
 }
